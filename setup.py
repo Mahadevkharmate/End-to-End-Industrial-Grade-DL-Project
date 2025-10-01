912f4b3f50863fe237add9a3c434a43e452e65c7
Mahadev Kharmate <mahadev.mk294@gmail.com>
2025-10-01 15:10:01 +0530

    setup.py-> updated

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
+# allows publishing your project to PyPi
+
 from setuptools import setup, find_packages
 
+#to show package related description like PyPi pkg we can read README.md file as long discription
+with open("README.md", "r", encoding = "utf-8") as f:
+    long_description = f.read()
+
+AUTHER_USER_NAME = "Mahadevkharmate"
+SRC_REPO = "CNNClassifier"
+AUTHER_EMAIL = "mahadev.mk294@gmail.com"
+
 setup(
-    name='cnn-classifier-industrial',
-    version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    name='End-to-End-Industrial-Grade-DL-Project',
+    version='0.1.0', #version of your project/model
+    author=AUTHER_USER_NAME,
+    author_email=AUTHER_EMAIL,
     description='An industrial-grade CNN classifier for image classification tasks.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/cnn-classifier-industrial',
+    url='https://github.com/Mahadevkharmate/End-to-End-Industrial-Grade-DL-Project',
+    project_urls={
+        "Bug Tracker": f"https://github.com/Mahadevkharmate/End-to-End-Industrial-Grade-DL-Project/issues"
+    },
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
